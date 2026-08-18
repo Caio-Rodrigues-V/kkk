25c874fdb56d1c32ee14aac9c42058bdb96f7109
Caio Rodrigues <caiov@example.com>
2026-08-17 21:37:42 -0300

    chore: debug loop start

--- public/app.js
+++ public/app.js
@@ -1209,6 +1209,8 @@ function renderInstagramTab() {
     if(bestPostContainer) bestPostContainer.classList.add("hidden");
     return;
   }
+  
+  tableBody.innerHTML = `<tr><td colspan="10">DEBUG: STARTING LOOP WITH ${media.length} ITEMS</td></tr>`;
 
   // Find best engaged post
   let bestPost = media[0];
